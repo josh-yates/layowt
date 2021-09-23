be2c9922fd6925fc8c5fb31a72603bddbb1a7b06
Joshua Yates <joshua.yates@enable.com>
2021-09-23 12:51:28 +0100

    Remove unnecessary test

--- test/services/commandService.test.ts
+++ test/services/commandService.test.ts
@@ -99,10 +99,5 @@ describe('CommandService', () => {
             treeNodeStore.split(treeNodeStore.getRootNode(), SplitType.Vertical);
             expect(sut.getCommand()).toBe('wt `; sp -V `; mf left');
         });
-
-        it('Escapes quotes correctly', () => {
-            // TODO
-            expect(true).toBe(false);
-        })
     });
 });
