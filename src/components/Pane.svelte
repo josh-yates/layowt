10488eb1682535adc05314202817ac7c96fe0af3
Joshua Yates <joshua.yates@enable.com>
2021-09-23 12:51:16 +0100

    Fix lagging input

--- src/components/Pane.svelte
+++ src/components/Pane.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
     import { createEventDispatcher } from "svelte";
-import type { TreeNode } from "../models/treeNode";
+    import type { TreeNode } from "../models/treeNode";
     export let pane: TreeNode;
     export let style: string;
     const dispatch = createEventDispatcher();
     const splitVertical = () => dispatch("splitVertical");
     const splitHorizontal = () => dispatch("splitHorizontal");
-    const input = () => dispatch('input');
+    const input = () => dispatch("input");
 </script>
 
 <div class="pane" {style}>
     <div class="command-holder">
-        <input on:input="{input}" bind:value={pane.content} />
+        <input bind:value={pane.content} on:input={input} />
     </div>
     <button class="vertical" on:click={splitVertical}>Split vertical</button>
     <button class="horizontal" on:click={splitHorizontal}
